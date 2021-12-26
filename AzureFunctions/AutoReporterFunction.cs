e7695ae5ae4dd6918c4c2e0e4e967a847373a882
fahminlb33 <fahminlb33@gmail.com>
2021-12-26 15:16:27 +0700

    Add dashboard page

--- AzureFunctions/AutoReporterFunction.cs
+++ AzureFunctions/AutoReporterFunction.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using FahmiNotionAutomation.Domain;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +14,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 namespace FahmiNotionAutomation
-{ 
+{
     public class AutoReporterFunction
     {
         private readonly IAutoReporterDomain _domain;
@@ -19,25 +24,110 @@ namespace FahmiNotionAutomation
             _domain = domain;
         }
 
-        //[FunctionName("ServeDashboardHtml")]
-        //public async Task<IActionResult> ServeDashboardHtml([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "/")] HttpRequest req, ILogger log)
-        //{
-        //    log.LogInformation("C# HTTP trigger function processed a request.");
+        [FunctionName("ServeDashboardHtml")]
+        public HttpResponseMessage ServeDashboardHtml([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "dashboard")] HttpRequest req, ILogger log, ExecutionContext context)
+        {
+            log.LogInformation("Dashboard page requested");
 
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            using var stream = new FileStream(Path.Combine(context.FunctionAppDirectory, "graph.html"), FileMode.Open);
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
 
-
-        //    return new OkObjectResult(responseMessage);
-        //}
+            log.LogInformation("Dashboard page loaded");
+            return response;
+        }
 
         [FunctionName("ServeDashboardData")]
         public async Task<IActionResult> ServeDashboardData([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "data")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Dashboard data requested");
 
+            var performances = await _domain.GetAllPerformance();
+            var performanceChartData = new List<ChartPointDto>();
+            if (performances != null)
+            {
+                performanceChartData = new List<ChartPointDto>
+                {
+                    new ChartPointDto
+                    {
+                        Type = "scatter",
+                        Name = "2-Day MA",
+                        XData = performances.Select(x => x.CurrentPeriod.Period).ToList(),
+                        YData = performances.Select(x => x.CommitmentMovingAverage).ToList(),
+                    },
+                    new ChartPointDto
+                    {
+                        Type = "bar",
+                        Name = "Commitment",
+                        XData = performances.Select(x => x.CurrentPeriod.Period).ToList(),
+                        YData = performances.Select(x => Convert.ToDouble(x.CurrentPeriod.Commitment)).ToList(),
+                    },
+                };
+            }
+
+            var statistics = await _domain.GetAllStatistics();
+            var statisticsChartData = new List<ChartPointDto>();
+            var latestStatisticsChartData = new List<PieChartPointDto>();
+            if (statistics != null)
+            {
+                statisticsChartData = new List<ChartPointDto>
+                {
+                    new ChartPointDto
+                    {
+                        Type = "scatter",
+                        Name = "Todo",
+                        XData = statistics.Select(x => x.Period).ToList(),
+                        YData = statistics.Select(x => Convert.ToDouble(x.Todo)).ToList(),
+                    },
+                    new ChartPointDto
+                    {
+                        Type = "scatter",
+                        Name = "In progress",
+                        XData = statistics.Select(x => x.Period).ToList(),
+                        YData = statistics.Select(x => Convert.ToDouble(x.InProgress + x.Review)).ToList(),
+                    },
+                    new ChartPointDto
+                    {
+                        Type = "scatter",
+                        Name = "Completed",
+                        XData = statistics.Select(x => x.Period).ToList(),
+                        YData = statistics.Select(x => Convert.ToDouble(x.Completed)).ToList(),
+                    },
+                };
+
+                var latestStatistics = statistics.Last();
+                latestStatisticsChartData = new List<PieChartPointDto>
+                {
+                    new PieChartPointDto
+                    {
+                        Type = "pie",
+                        Labels = new List<string> {
+                            "Backlog",
+                            "Triage",
+                            "Todo",
+                            "InProgress",
+                            "Review",
+                            "Completed",
+                        },
+                        Values = new List<double> {
+                            latestStatistics.Backlog,
+                            latestStatistics.Triage,
+                            latestStatistics.Todo,
+                            latestStatistics.InProgress,
+                            latestStatistics.Review,
+                            latestStatistics.Completed
+                        }
+                    }
+                };
+            }
+
+            log.LogInformation("Dashboard data processed");
             return new JsonResult(new
             {
-                performances = await _domain.GetAllPerformance(),
-                statistics = await _domain.GetAllStatistics(),
+                performances = performanceChartData,
+                statistics = statisticsChartData,
+                latestStatistics = latestStatisticsChartData
             });
         }
 
@@ -50,6 +140,6 @@ namespace FahmiNotionAutomation
 
             log.LogInformation($"Notion automation function executed at: {DateTime.Now}");
         }
-        
+
     }
 }
---
+++ Domain/ChartPointDto.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace FahmiNotionAutomation.Domain
+{
+    public record PieChartPointDto
+    {
+        [JsonProperty("values")]
+        public IList<double> Values { get; set; }
+
+        [JsonProperty("labels")]
+        public IList<string> Labels { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public record ChartPointDto
+    {
+        [JsonProperty("x")]
+        public IList<DateTime> XData { get; set; }
+
+        [JsonProperty("y")]
+        public IList<double> YData { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
